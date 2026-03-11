624b135e627d94c75ddf29b4e3ee82465d7a4131
issam.hosni <issam.hosni@univ-tiaret.dz>
2026-03-11 14:28:32 +0100

    feat(comments): completed comments resource

--- src/components/comments/controller.ts
+++ src/components/comments/controller.ts
@@ -1,19 +1,89 @@
+import type { Request, Response } from "express";
 import { BaseController } from "../../utils/base_controller.js";
+import { CommentsService } from "./service.js";
+import { TasksUtil } from "../tasks/controller.js";
+import { UsersUtil } from "../users/controller.js";
+
+export class CommentsUtil {
+  public static async checkValidCommentsIds(comments_ids: string[]) {
+    const commentsService = new CommentsService();
+    const comments = await commentsService.findByIds(comments_ids);
+    return comments.data?.length === comments_ids.length;
+  }
+}
 
 export class CommentController extends BaseController {
-  public addHandler() {
-    // addHandler
+  public async addHandler(req: Request, res: Response) {
+    try {
+      const service = new CommentsService();
+      const comment = req.body;
+
+      if (!comment.task_id) {
+        res.status(400).json({
+          statusCode: 400,
+          status: "error",
+          message: "task_id is required",
+        });
+        return;
+      }
+
+      const isValidTask = await TasksUtil.checkValidTasksIds([
+        comment.task_id as string,
+      ]);
+      if (!isValidTask) {
+        res.status(400).json({
+          statusCode: 400,
+          status: "error",
+          message: "invalid task_id",
+        });
+        return;
+      }
+
+      if (comment.user_id) {
+        const isValidUser = await UsersUtil.checkValidUserIDs([
+          comment.user_id as string,
+        ]);
+
+        if (!isValidUser) {
+          res.status(400).json({
+            statusCode: 400,
+            status: "error",
+            message: "invalid user_id",
+          });
+          return;
+        }
+      }
+
+      const createdComment = await service.create(comment);
+      res.status(201).json(createdComment);
+    } catch (error: any) {
+      console.error("error while creating comment: ", error.message);
+      res.status(500).json({
+        statusCode: 500,
+        status: "error",
+        message: "internal server error",
+      });
+    }
   }
-  public getAllHandler() {
-    // getAllHandler
+  public async getAllHandler(req: Request, res: Response) {
+    const service = new CommentsService();
+    const result = await service.findAll(req.query);
+    res.status(result.statusCode as number).json(result);
   }
-  public getOneHandler() {
-    // getDetailsHandler
+  public async getOneHandler(req: Request, res: Response) {
+    const service = new CommentsService();
+    const result = await service.findOne(req.params.id as string);
+    res.status(result.statusCode as number).json(result);
   }
-  public async updateHandler() {
-    // updateHandler
+  public async updateHandler(req: Request, res: Response) {
+    const comment = req.body;
+    const service = new CommentsService();
+    const result = await service.update(req.params.id as string, comment);
+    res.status(result.statusCode as number).json(result);
   }
-  public async deleteHandler() {
-    // deleteHandler
+  public async deleteHandler(req: Request, res: Response) {
+    const service = new CommentsService();
+    const result = await service.delete(req.params.id as string);
+    res.status(result.statusCode as number).json(result);
   }
 }
--- src/components/comments/route.ts
+++ src/components/comments/route.ts
@@ -1,5 +1,36 @@
 import type { Express } from "express";
+import { authorize } from "../../utils/auth_utils.js";
 import { CommentController } from "./controller.js";
+import { body } from "express-validator";
+import { validate } from "../../utils/validator.js";
+
+const validCommentInput = [
+  body("comment").trim().notEmpty().withMessage("comment is required"),
+  body("task_id").trim().notEmpty().withMessage("task ID is required"),
+  body("user_id")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("user ID must not be empty if provided"),
+];
+
+const validCommentInputPatch = [
+  body("comment")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("comment text must not be empty"),
+  body("task_id")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("task ID must not be empty"),
+  body("user_id")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("user ID must not be empty"),
+];
 
 export class CommentRoutes {
   private basePoint = "/api/comments";
@@ -8,13 +39,15 @@ export class CommentRoutes {
 
     app
       .route(this.basePoint)
-      .post(controller.addHandler)
+      .all(authorize)
+      .post(validate(validCommentInput), controller.addHandler)
       .get(controller.getAllHandler);
 
     app
       .route(this.basePoint + "/:id")
+      .all(authorize)
       .delete(controller.deleteHandler)
-      .patch(controller.updateHandler)
+      .patch(validate(validCommentInputPatch), controller.updateHandler)
       .get(controller.getOneHandler);
   }
 }
